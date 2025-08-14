4897aa2e397a4ccefaeb672e446bb7d6054203a1
Nitin Patel <156987426+nitin-pat@users.noreply.github.com>
2025-08-14 13:44:59 +0530

    first commit

---
+++ scrypt.js
@@ -0,0 +1,29 @@
+  const toggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
+
+  const quotes = [
+  "Teaching is the one profession that creates all other professions",
+  "Education is the most powerful weapon which you can use to change the world",
+  "The mind is not a vessel to be filled, but a fire to be kindled.",
+  "Children must be taught how to think, not what to think",
+  "Learning is a treasure that will follow its owner everywhere"
+];
+
+let index = 0;
+const quoteElement = document.getElementById("quote-text");
+
+function changeQuote() {
+  index = (index + 1) % quotes.length;
+  quoteElement.style.opacity = 0;
+  setTimeout(() => {
+    quoteElement.textContent = quotes[index];
+    quoteElement.style.opacity = 1;
+  }, 500);
+}
+
+setInterval(changeQuote, 4000);
